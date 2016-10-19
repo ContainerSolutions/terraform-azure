bac4297a540b7b366ba4ef8f3e4994ab7e8743ab
Lukasz Guminski <lguminski@gmail.com>
2016-10-19 17:42:47 +0200

    randomizing name

--- StorageProvider.tf
+++ StorageProvider.tf
@@ -1,5 +1,5 @@
 resource "azure_storage_service" "azure_test_storage" {
-    name = "storage"
+    name = "storage${random_id.server.hex}"
     location = "North Europe"
     description = "Made by Terraform."
     account_type = "Standard_LRS"
